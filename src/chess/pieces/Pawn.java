0df4f0dd37bfb728be9b143c408b0c2a26f268b6
simonloach <mrtheyouis@gmail.com>
2020-01-04 21:54:39 +0100

    pawn added, small fixes on others

---
+++ src/chess/pieces/Pawn.java
@@ -0,0 +1,36 @@
+package chess.pieces;
+
+import chess.Alliance;
+import chess.board.Board;
+import chess.board.BoardUtils;
+import chess.board.Move;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Pawn extends Piece{
+    private final static int[] CANDIDATE_MOVE_COORDINATE = {8};
+
+    Pawn(int piecePosition, Alliance pieceAlliance) {
+        super(piecePosition, pieceAlliance);
+    }
+
+    @Override
+    public Collection<Move> calculateLegalMoves(Board board) {
+
+        final List<Move> legalMoves = new ArrayList<>();
+        for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE){
+            int candidateDestinationCoordinate = this.piecePosition + (this.getPieceAlliance().getDirection() * currentCandidateOffset);
+            if(!BoardUtils.isValidCandidate(candidateDestinationCoordinate)){
+                continue;
+            }
+            if (currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isOccupied()){
+                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationCoordinate)); // MORE WORK TO DO HERE
+            }
+
+
+        }
+        return legalMoves;
+    }
+}
