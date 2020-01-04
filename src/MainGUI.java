366e1c726398d975ac0ade0ce697dd4b18786620
SzumiecM <milosz.sz.m@gmail.com>
2020-01-04 20:39:10 +0100

    colors test

--- src/MainGUI.java
+++ src/MainGUI.java
@@ -1,13 +1,21 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainGUI extends JFrame{
     private JPanel PANEL;
     private List<JPanel> panelList = new ArrayList<>();
-    private String[] pola = {"a8", "a7", "a6", "a5", "a4", "a3", "a2", "a1",
-            "b8", "b7", "b6", "b5", "b4", "b3", "b2", "b1"};
+    private String[] pola = {"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
+            "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
+            "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6",
+            "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5",
+            "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
+            "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
+            "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
+            "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"};
 
     public void setPanelColor(Container parent) //Działa i to się liczy
     {
@@ -15,9 +23,9 @@ public class MainGUI extends JFrame{
         {
             if(c instanceof Container)
             {
-                if(c instanceof JPanel)
+                if(c.getName() == "a8")
                 {
-                    c.setBackground(Color.BLACK);
+                    c.setBackground(Color.RED);
                 }
 
                 setPanelColor((Container)c);
@@ -31,28 +39,51 @@ public class MainGUI extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         int colorcheck = 1;
         int colorswap = 1;
-        for(int i=0; i<16; i++){
-            JPanel x = new JPanel();
+        for(int i=0; i<64; i++){
+            panelList.add(new JPanel());
             JLabel lab = new JLabel(pola[i]);
-            x.setName(pola[i]);
-            x.add(lab);
+            panelList.get(i).setName(pola[i]);
+
             if(colorcheck%2 == colorswap%2){
-                x.setBackground(Color.WHITE);
+                panelList.get(i).setBackground(Color.WHITE);
+                lab.setForeground(Color.BLACK);
             }
             else{
-                x.setBackground(Color.BLACK);
+                panelList.get(i).setBackground(Color.BLACK);
+                lab.setForeground(Color.WHITE);
             }
             if(colorcheck == 8){
                 colorcheck = 1;
                 colorswap += 1;
             }
-            add(x);
+            else {
+                colorcheck += 1;
+            }
+            panelList.get(i).add(lab);
+            add(panelList.get(i));
         }
 
-
+        //setPanelColor(this);
         pack();
     }
 
+    private abstract class PanelListener implements MouseListener {
+
+        @Override
+        public void mouseClicked(MouseEvent event) {
+            /* source is the object that got clicked
+             *
+             * If the source is actually a JPanel,
+             * then will the object be parsed to JPanel
+             * since we need the setBackground() method
+             */
+            Object source = event.getSource();
+            if (source instanceof JPanel) {
+                JPanel panelPressed = (JPanel) source;
+                panelPressed.setBackground(Color.blue);
+            }
+        }
+    }
 
     public static void main(String[] args){
         /*JFrame frame = new JFrame("CHESS");
