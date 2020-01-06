7501593ad33375c180c9192930aa49814345fecd
simonloach <mrtheyouis@gmail.com>
2020-01-06 01:48:23 +0100

    adding piece images

--- src/chess/gui/Table.java
+++ src/chess/gui/Table.java
@@ -1,21 +1,28 @@
 package chess.gui;
 
+import chess.board.Board;
 import chess.board.BoardUtils;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Table {
     private final JFrame gameFrame;
     private final BoardPanel boardPanel;
+    private final Board chessBoard;
 
     private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
     private static final Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 350);
     private static final Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+    private static String DEFAULT_IMAGE_PATH = "src/chess/images/";
 
     private final Color lightTileColor = Color.decode("#FF0000");
     private final Color darkTileColor = Color.decode("#FFFF00");
@@ -26,10 +33,9 @@ public class Table {
         final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameFrame.setJMenuBar(tableMenuBar);
         this.gameFrame.setSize(OUTER_FRAME_DIMENSION);
-
+        this.chessBoard = Board.createStandardBoard();
         this.boardPanel = new BoardPanel();
         this.gameFrame.add(this.boardPanel, BorderLayout.CENTER);
-
         this.gameFrame.setVisible(true);
 
     }
@@ -82,6 +88,7 @@ public class Table {
     }
 
     private class TilePanel extends JPanel {
+
         private final int tileId;
 
         TilePanel(final BoardPanel boardPanel,
@@ -90,21 +97,32 @@ public class Table {
             this.tileId = tileId;
             setPreferredSize(TILE_PANEL_DIMENSION);
             assignTileColor();
+            assignTilePieceIcon(chessBoard);
             validate();
+        }
 
-
+        private void assignTilePieceIcon(final Board board){
+            this.removeAll();
+            if(board.getTile(this.tileId).isOccupied()){
+                try {
+                    final BufferedImage image = ImageIO.read(new File(DEFAULT_IMAGE_PATH + board.getTile(this.tileId).getPiece().getPieceAlliance().toString().substring(0,1) + board.getTile(this.tileId).getPiece().toString() + ".gif"));
+                    add(new JLabel(new ImageIcon(image)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
         private void assignTileColor() {
-            if(BoardUtils.FIRST_ROW[tileId] ||
-                    BoardUtils.THIRD_ROW[tileId]||
+            if (BoardUtils.FIRST_ROW[tileId] ||
+                    BoardUtils.THIRD_ROW[tileId] ||
                     BoardUtils.FIFTH_ROW[tileId] ||
-                    BoardUtils.SEVENTH_ROW[tileId]){
+                    BoardUtils.SEVENTH_ROW[tileId]) {
                 setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
             } else if (BoardUtils.SECOND_ROW[tileId] ||
-                    BoardUtils.FOURTH_ROW[tileId]||
+                    BoardUtils.FOURTH_ROW[tileId] ||
                     BoardUtils.SIXTH_ROW[tileId] ||
-                    BoardUtils.EIGHTH_ROW[tileId]){
+                    BoardUtils.EIGHTH_ROW[tileId]) {
                 setBackground(this.tileId % 2 == 0 ? darkTileColor : lightTileColor);
             }
         }
